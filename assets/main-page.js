b907680147ce6a2c11bbebcd50cebbe0eec1f040
Mani Teja Nethi <nethimaniteja64@gmail.com>
2024-04-10 20:50:34 +0530

    toggle done

--- assets/main-page.js
+++ assets/main-page.js
@@ -1,4 +1,11 @@
-function myExternalFunction(sectionSettings) {
-    return sectionSettings.section_header;
-  }
-
+let allCaterogie = document.getElementById('allCaterogies-Condoms');
+allCaterogie.style.display = "flex";
+function changeValue(val){
+console.log(val)
+let all = document.querySelectorAll('[id^=allCaterogies]');
+all.forEach(each=>{
+    each.style.display = "none";
+})
+let allCaterogies = document.getElementById(`allCaterogies-${val}`);
+allCaterogies.style.display = "flex";
+}
