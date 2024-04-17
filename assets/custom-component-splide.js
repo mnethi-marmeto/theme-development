b5d52fd8aea7ded585e2d9a36b886d455ea3072a
Mani Teja Nethi <nethimaniteja64@gmail.com>
2024-04-17 23:08:21 +0530

    all

--- assets/custom-component-splide.js
+++ assets/custom-component-splide.js
@@ -10,12 +10,22 @@ class SplideComponent extends HTMLElement{
 
         let activeEl = document.querySelector("li.custom-testimonials.is-active");
         let activecard = document.querySelector("li.custom-testimonials.is-active div.custom-testimonials--slide")
+        let allcards = document.querySelectorAll("div.custom-testimonials--slide");
+        allcards.forEach(each=>{
+            let color = each.dataset.cardcolor;
+            each.style.backgroundColor = color;
+        })
         let activeCardcolor = activeEl.dataset.activecolor;
         activecard.style.backgroundColor = activeCardcolor;
 
        this.carousel.on('moved', function () {
         let activeEl = document.querySelector("li.custom-testimonials.is-active");
         let activecard = document.querySelector("li.custom-testimonials.is-active div.custom-testimonials--slide")
+        let allcards = document.querySelectorAll("div.custom-testimonials--slide");
+        allcards.forEach(each=>{
+            let color = each.dataset.cardcolor;
+            each.style.backgroundColor = color;
+        })
         let activeMainImage = activeEl.dataset.image;
         mainImage.setAttribute("src",activeMainImage);
         let activeCardcolor = activeEl.dataset.activecolor;
