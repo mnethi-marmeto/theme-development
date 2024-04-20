43aefdae65fb5e12d67ffa460aa7dfa1e565008a
Mani Teja Nethi <nethimaniteja64@gmail.com>
2024-04-20 17:22:57 +0530

    all

--- assets/custom-component-splide.js
+++ assets/custom-component-splide.js
@@ -1,42 +1,45 @@
-class SplideComponent extends HTMLElement{
-    constructor(){
+class SplideComponent extends HTMLElement {
+    constructor() {
         super();
         this.splider = this;
         this.options = JSON.parse(this.dataset.options);
-        this.initalizeSplide()
+        this.initializeSplide();
     }
-    initalizeSplide(){
-       this.carousel =  new Splide(this.splider,this.options).mount();
 
-        let activeEl = document.querySelector("li.custom-testimonials.is-active");
-        let activecard = document.querySelector("li.custom-testimonials.is-active div.custom-testimonials--slide")
-        let allcards = document.querySelectorAll("div.custom-testimonials--slide");
-        allcards.forEach(each=>{
+    initializeSplide() {
+        this.carousel = new Splide(this.splider, this.options).mount();
+        this.updateSlideColors();
+        this.carousel.on('moved', () => {
+            this.updateSlideColors();
+            this.updateMainImage();
+        });
+    }
+
+    updateSlideColors() {
+        const activeEl = document.querySelector("li.custom-testimonials.is-active");
+        const activeCard = activeEl.querySelector("div.custom-testimonials--slide");
+        const allCards = document.querySelectorAll("div.custom-testimonials--slide");
+
+        allCards.forEach(each => {
             let color = each.dataset.cardcolor;
             each.style.backgroundColor = color;
-        })
-        let activeCardcolor = activeEl.dataset.activecolor;
-        activecard.style.backgroundColor = activeCardcolor;
+        });
 
-       this.carousel.on('moved', function () {
-        let activeEl = document.querySelector("li.custom-testimonials.is-active");
-        let activecard = document.querySelector("li.custom-testimonials.is-active div.custom-testimonials--slide")
-        let allcards = document.querySelectorAll("div.custom-testimonials--slide");
-        allcards.forEach(each=>{
-            let color = each.dataset.cardcolor;
-            each.style.backgroundColor = color;
-        })
-        let activeMainImage = activeEl.dataset.image;
-        mainImage.setAttribute("src",activeMainImage);
-        let activeCardcolor = activeEl.dataset.activecolor;
-        activecard.style.backgroundColor = activeCardcolor;
-    });
+        const activeCardColor = activeEl.dataset.activecolor;
+        activeCard.style.backgroundColor = activeCardColor;
+    }
+
+    updateMainImage() {
+        const activeEl = document.querySelector("li.custom-testimonials.is-active");
+        const mainImage = document.getElementById("mainImage");
+        const activeMainImage = activeEl.dataset.image;
+
+        mainImage.setAttribute("src", activeMainImage);
     }
 }
 
-customElements.define("splide-component", SplideComponent);
 
-let mainImage = document.getElementById("mainImage");
+customElements.define("splide-component", SplideComponent);
 
 
   
