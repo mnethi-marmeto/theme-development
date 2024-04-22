fca291a15ceb0040d317d27b98dd00922e9fdc93
Mani Teja Nethi <nethimaniteja64@gmail.com>
2024-04-22 11:50:44 +0530

    footer

--- assets/practice-featured.js
+++ assets/practice-featured.js
@@ -2,7 +2,6 @@ function append(){
     fetch("/?section_id=template--15650135736410__multicolumn_nNHBYp")
     .then(response=>response.text())
     .then((responseText) => {
-        console.log(responseText)
         const html = new DOMParser().parseFromString(responseText,'text/html');
         const container = document.getElementById("appendContainer");
         const content = html.querySelector("body")
